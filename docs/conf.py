2673b72d895209d75eb5a652c5e9bb1c4254dac4
Artem Golubin <me@rushter.com>
2020-12-17 13:46:25 +0300

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ author = 'Mikhail Korobov'
 # built documents.
 #
 # The short X.Y version.
-version = '0.7.5'
+version = '0.7.6'
 # The full version, including alpha/beta/rc tags.
 release = version
 
